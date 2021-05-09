3536aca396bca19ea22d0851043154efc68047ed
enzonjagi <amosndungo@gmail.com>
2021-05-09 20:33:57 +0300

    Added an empty Sign in Screen

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:todo_for_teams/Screens/signIn.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: SignIn(),
+    );
+  }
+}
+
+
+
