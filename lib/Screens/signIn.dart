c6af368ad4b6a9dd115f5ca6cef78bdfadd1f2ce
enzonjagi <amosndungo@gmail.com>
2021-05-10 14:38:16 +0300

    Simpler Signup: Yet To Add Linear Gradient

--- lib/Screens/signIn.dart
+++ lib/Screens/signIn.dart
@@ -1,5 +1,6 @@
-//import 'dart:html';
 
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -19,8 +20,7 @@ class _SignInState extends State<SignIn> {
           child: Column(
             children: [
 
-              ///Blend Image with Scaffold
-              Image.asset('assets/team.jpg'),
+              ///Add a linear gradient with Image
               Spacer(),
               Text(
                   "Welcome to kaziManager for Teams",
@@ -30,83 +30,108 @@ class _SignInState extends State<SignIn> {
                   fontWeight: FontWeight.bold
                 ),
               ),
-              SizedBox(height: 10,),
+              SizedBox(height: 30,),
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
                 child:Form(
                   key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        //autofillHints: ["Email"],
-                        decoration: const InputDecoration(
-                          hintText: "Email"
-                        ),
-                        validator: (value){
-                          if(value==null || value.isEmpty){
-                            return "Please Enter Your Email address";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 10,),
-                      TextFormField(
-                        //autofillHints: ["Email"],
-                        decoration: const InputDecoration(
-                            hintText: "Password",
-                        ),
-                        obscureText: true,
-                        validator: (value){
-                          if(value==null || value.isEmpty){
-                            return "Please Enter Your password";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 30,),
-
-                      //Make button
-                      Container(
-                        height: 50.0,
-                        width: 200,
-                        margin: EdgeInsets.all(10),
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(color: Colors.brown)
-                              )
-                            )
+                  child: Expanded(
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          //autofillHints: ["Email"],
+                          decoration: const InputDecoration(
+                            hintText: "Email"
                           ),
-                          onPressed: (){
-
+                          validator: (value){
+                            if(value==null || value.isEmpty){
+                              return "Please Enter Your Email address";
+                            }
+                            return null;
                           },
-                          child: Text(
-                            "SignIn",
-                            style: TextStyle(
-                              fontSize: 20
-                            ),
+                        ),
+                        SizedBox(height: 10,),
+                        TextFormField(
+                          //autofillHints: ["Email"],
+                          decoration: const InputDecoration(
+                              hintText: "Password",
                           ),
+                          obscureText: true,
+                          validator: (value){
+                            if(value==null || value.isEmpty){
+                              return "Please Enter Your password";
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 30,),
+
+                        //Make button
+                        Container(
+                          height: 45.0,
+                          width: 300,
+                          margin: EdgeInsets.all(10),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                  side: BorderSide(color: Colors.brown)
+                                )
+                              )
+                            ),
+                            onPressed: (){
+
+                            },
+                            child: Text(
+                              "SignIn",
+                              style: TextStyle(
+                                fontSize: 20
+                              ),
+                            ),
+
+                          ),
+                        ),
+                        SizedBox(height: 1,),
+                        Container(
+                          height: 40.0,
+                          width: 300,
+                          margin: EdgeInsets.all(10),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(18.0),
+                                        side: BorderSide(color: Colors.transparent)
+                                    )
+                                ),
+                              backgroundColor: MaterialStateProperty.resolveWith((states) => null)
+                              
+                            ),
+                            onPressed: (){
+
+                            },
+                            child: Text(
+                              "Register",
+                              style: TextStyle(
+                                  fontSize: 20
+                              ),
+                            ),
+
+                          ),
+                        ),
+                        TextButton(
+                                onPressed: (){},
+                                child: Text("Forgot Password?"),
 
                         ),
-                      ),
-                      SizedBox(height: 10,),
-                      TextButton(
-                          onPressed: (){},
-                          child: Text(
-                            "Register",
-                            style: TextStyle(
-                                fontSize: 20
-                            ),
-                          ),
-                      ),
-                      SizedBox(height: 20,),
+                        SizedBox(height: 10,),
 
 
 
 
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               )
